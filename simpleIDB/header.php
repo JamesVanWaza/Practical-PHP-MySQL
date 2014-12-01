dec809e263b444a09937e439245bfabd111d837e
JamesVanWaza <jgacuca@gmail.com>
2014-12-01 16:57:39 -0500

    Added the side-nav class for the Registration Menu

--- simpleIDB/header.php
+++ simpleIDB/header.php
@@ -2,7 +2,7 @@
 <body>
     <div id="header">
     <h1>This is the Header</h1>
-    <nav>
+    <nav class="side-nav">
         <ul>
             <li>
                 <a href="register-page.php">Register</a>
