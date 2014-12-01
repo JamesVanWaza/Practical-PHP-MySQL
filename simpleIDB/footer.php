50c1a7746b45cce7d217925a0385ca9d6648be5f
JamesVanWaza <jgacuca@gmail.com>
2014-12-01 16:31:17 -0500

    Changed the Footer Tag

--- simpleIDB/footer.php
+++ simpleIDB/footer.php
@@ -1,8 +1,8 @@
 <?php include('header.php'); ?>
-<div id="footer">
+<footer>
 <p>Copyright &copy; Adrian West 2012  Designed by image
 
 <a"http://www.colycomputerhelp.co.uk/">Adrian West </a>  Valid image
 <a"http://jigsaw.w3.org/css-validator/">CSS</a> &amp; image
 <a"http://validator.w3.org/">HTML5</a></p>
-</div>
+</footer>
