2e005c7ebe5e0d4f941ed2ff2b6a6e8ba244bbdc
JamesVanWaza <jgacuca@gmail.com>
2014-12-01 16:52:04 -0500

    Modified the footer to remove the header include files

--- simpleIDB/footer.php
+++ simpleIDB/footer.php
@@ -1,8 +1,9 @@
-<?php include('header.php'); ?>
 <footer>
-<p>Copyright &copy; Adrian West 2012  Designed by image
-
-<a"http://www.colycomputerhelp.co.uk/">Adrian West </a>  Valid image
-<a"http://jigsaw.w3.org/css-validator/">CSS</a> &amp; image
-<a"http://validator.w3.org/">HTML5</a></p>
+	<p>Copyright &copy; Adrian West 2012  Designed by image
+	<br>
+	<a"http://www.colycomputerhelp.co.uk/">Adrian West </a>
+	<br>
+	<a"http://jigsaw.w3.org/css-validator/">CSS</a>
+	<br>
+	<a"http://validator.w3.org/">HTML5</a></p>
 </footer>
--- simpleIDB/info-col.php
+++ simpleIDB/info-col.php
@@ -1,5 +1,3 @@
-<?php include('header.php'); ?>
-
 <div id="info-col">
 <h3>This is the information column</h3>
     <p>Web design by <br>A W West</p><p>&nbsp;</p>
