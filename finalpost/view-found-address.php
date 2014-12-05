3dfc3586a5566575ffad9caf77be1e2042276bb5
JamesVanWaza <jgacuca@gmail.com>
2014-12-05 14:02:39 -0500

    Added Admintable Folder and FInalPost View - Found - Address Page

---
+++ finalpost/view-found-address.php
@@ -0,0 +1,68 @@
+<?php include('nav.php'); ?>
+<?php
+	session_start();
+	if (!isset($_SESSION['user_level']) or($_SESSION['user_level']) !=1) {
+	    header('Location: login.php');
+		exit();
+	}
+?>
+<?php
+// This script fetches selected records from the users table
+    require ('mysqli-connect.php'); // Connect to the database
+    echo '<p>If no record is shown, this is because of an incorrect or missing entry in
+the search form.<br>Click the Search button and try again</p>';
+$fname=$_POST['fname'];
+$fname = mysqli_real_escape_string($dbcon, $fname
+$lname=$_POST['lname'];
+$lname = mysqli_real_escape_string($dbcon, $lname);
+$q = "SELECT title, lname, fname, addr1, addr2, city, county, pcode, phone, user_id
+FROM finalpost WHERE lname='$lname' AND fname='$fname' ";
+$result = @mysqli_query ($dbcon, $q); // Run the query
+if ($result) { // If it ran, display the records
+// Table headings                                                                        #2
+echo '<table>
+<tr><td><b>Edit</b></td>
+<td><b>Title</b></td>
+<td><b>Last Name</b></td>
+<td><b>First Name</b></td>
+<td><b>Addrs1</b></td>
+<td><b>Addrs2</b></td>
+<td><b>City</b></td>
+<td><b>County</b></td>
+<td><b>Pcode</b></td>
+<td><b>Phone</b></td>
+</tr>';
+// Fetch and display the records
+while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+    echo '<tr>
+    <td><a href="edit_address.php?id=' . $row['user_id'] . '">Edit</a></td>
+<td>' . $row['title'] . '</td>
+<td>' . $row['lname'] . '</td>
+<td>' . $row['fname'] . '</td>
+<td>' . $row['addr1'] . '</td>
+<td>' . $row['addr2'] . '</td>
+<td>' . $row['city'] . '</td>
+<td>' . $row['county'] . '</td>
+<td>' . $row['pcode'] . '</td>
+<td>' . $row['phone'] . '</td>
+    </tr>';
+    }
+    echo '</table>'; // Close the table
+    mysqli_free_result ($result); // Free up the resources
+} else { // If it failed to run
+// Display message
+echo '<p class="alert-box alert round">The current users could not be retrieved. We apologize for
+any inconvenience.</p>';
+// Debugging message
+    echo '<p>' . mysqli_error($dbcon) . '<br><br>Query: ' . $q . '</p>';
+} // End of the if ($result)
+//Now display a figure for the total number of records/members
+$q = "SELECT COUNT(user_id) FROM finalpost";
+$result = @mysqli_query ($dbcon, $q);
+$row = @mysqli_fetch_array ($result, MYSQLI_NUM);
+$members = $row[0];
+mysqli_close($dbcon); //Close the database connection
+echo "<p>Total membership: $members</p>";
+?>
+</div><!-- End of found address page -->
+<?php include("footer.php"); ?>
