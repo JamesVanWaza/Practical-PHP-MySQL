586c6a2b7e2628378a7ca97a440051e339df5a70
JamesVanWaza <jgacuca@gmail.com>
2014-12-05 15:05:09 -0500

    Added LMN Migrate Folder

---
+++ lmnmigrate/header.php
@@ -0,0 +1,14 @@
+<?php include('html5req.php'); ?>
+<body>
+    <div id="header">
+    <h1>This is the Header</h1>
+    <nav>
+        <ul class="side-nav">
+            <li>
+                <a href="login.php">Login</a>
+                <a href="register-page.php">Register</a>
+            </li>
+        </ul>
+    </nav>
+</div>
+</body>
---
+++ lmnmigrate/password-header.php
@@ -0,0 +1,14 @@
+<?php include('html5req.php'); ?>
+<body>
+    <div id="header">
+    <h1>This is the Header</h1>
+    <nav>
+        <ul class="side-nav">
+            <li>
+                <a href="register-page.php">Erase Entries</a>
+                <a href="index.php">Cancel</a>
+            </li>
+        </ul>
+    </nav>
+</div>
+</body>
