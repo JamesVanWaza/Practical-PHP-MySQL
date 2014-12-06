cdc6c62f4039031a94e017ba7da730b76f895a57
JamesVanWaza <jgacuca@gmail.com>
2014-12-05 21:10:24 -0500

    Added the Contact Page and the Description Page

---
+++ estate/description.php
@@ -0,0 +1,7 @@
+<?php include('header.php');?>
+<h3>To Arrange a viewing please click the Contact Us Button and Quote the Reference Number</h3>
+<p>
+	<span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam excepturi sequi ullam tempore temporibus unde et dolor quis consequuntur eius natus, non odit sapiente, explicabo possimus a magni. Deleniti, vero.</span>
+	<span>Reprehenderit maxime perferendis nisi, enim nostrum repellendus. Assumenda perspiciatis distinctio officia ea, nihil amet. Suscipit dolorem reiciendis, mollitia nisi aspernatur, incidunt, earum modi neque culpa debitis recusandae quam fugiat libero.</span>
+	<span>Nam quod esse nemo, omnis rerum iste reprehenderit mollitia voluptatibus delectus quam facere vitae, nulla, atque expedita officia quibusdam amet. Qui vel provident nemo consequatur, deserunt natus, accusamus incidunt. Eligendi.</span>
+</p>
