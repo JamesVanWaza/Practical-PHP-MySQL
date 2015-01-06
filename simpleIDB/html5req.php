fe12cf69d8f60a8f64485a84ea2cd9f2c3fff7b1
James Van Waza <jgacuca@gmail.com>
2015-01-06 09:57:09 -0500

    Added another font from Google for the h2 header

--- simpleIDB/html5req.php
+++ simpleIDB/html5req.php
@@ -5,6 +5,7 @@
     <link href="css/foundation.css" rel="stylesheet" type="text/css">
     <link href='http://fonts.googleapis.com/css?family=Quattrocento:400,700|Quattrocento+Sans:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
     <link href="css/main.css" rel="stylesheet" type="text/css">
     <script src="js/vendor/jquery.js"></script>
     <script src="js/vendor/zepto.min.js"></script>
