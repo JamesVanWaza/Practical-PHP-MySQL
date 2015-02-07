85a132011e892e32530a4f220d92771d41d2f967
James Van Waza <jgacuca@gmail.com>
2015-02-06 22:15:31 -0500

    Removed repeated php tags

--- loginDB/members-page.php
+++ loginDB/members-page.php
@@ -1,5 +1,4 @@
-<?php include('header-members.php'); ?>
-<?php
+<?php include('header-members.php');
 session_start();
 if (!isset($_SESSION['user_level']) or($_SESSION['user_level']) !=0) {
 header('Location: login.php');
