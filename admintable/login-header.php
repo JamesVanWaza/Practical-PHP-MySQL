2afb84e58a11b4f08db4494bd53634b3dcdff8a4
James Van Waza <jgacuca@gmail.com>
2015-02-07 08:50:27 -0500

    Changed login header

--- admintable/login-header.php
+++ admintable/login-header.php
@@ -1,32 +1,13 @@
-<?php include('header.php'); ?>
-<nav class="top-bar" data-topbar role="navigation" data-options="is_hover: false">
-    <ul class="title-area">
-        <li class="name">
-            <h1><a href="index.php">Login IDB</a></h1>
-        </li>
-        <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
-        <li class="toggle-topbar menu-icon"><a href="#"><span>MENU</span></a>
-    </li>
-</ul>
-<section class="top-bar-section">
-    <!-- Right Nav Section -->
-    <ul class="right">
-        <li><a href="login.php">Erase Entries </a></li>
-        <li><a href="register-page.php">Register</a></li>
-        <li><a href="index.php">Cancel</a></li>
-    </ul>
-</section>
-</nav>
-<script>
-document.write('<script src=js/vendor/' +
-('__proto__' in {} ? 'zepto' : 'jquery') +
-'.js><\/script>')
-</script>
-<script src="js/vendor/jquery.js"></script>
-<script src="js/foundation/foundation.js"></script>
-<!-- <script src="js/foundation/foundation.orbit.js"></script> -->
-<!-- Updating slider settings in here -->
-<script src="js/foundation.min.js"></script>
-<script>
-$(document).foundation();
-</script>
+<?php include 'html5req.php';?>
+<body>
+    <div id="header">
+    <h1>This is the Header</h1>
+    <nav>
+        <ul class="side-nav">
+            <li><a href="login.php">Erase Entries </a></li>
+            <li><a href="register-page.php">Register</a></li>
+            <li><a href="index.php">Cancel</a></li>
+        </ul>
+    </nav>
+</div>
+</body>
