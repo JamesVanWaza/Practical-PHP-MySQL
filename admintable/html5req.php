e1d06c708b0ea5f5db02e4aa53c012eb49509760
James Van Waza <jgacuca@gmail.com>
2015-02-07 09:10:05 -0500

    Added the missing body tags

--- admintable/html5req.php
+++ admintable/html5req.php
@@ -7,11 +7,13 @@
     <link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
     <link href="css/main.css" rel="stylesheet" type="text/css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.js"></script>
+</head>
+<body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.js"></script>
     <script src="http://cdn.foundation5.zurb.com/foundation.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
     <script>
         $(document).foundation();
     </script>
-</head>
-<body>
+</body>
+</html>
