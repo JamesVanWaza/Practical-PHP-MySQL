4071adfcfd7ae9df0b214f9b1346d03a73f8de9d
James Van Waza <jgacuca@gmail.com>
2015-02-06 21:40:36 -0500

    Removed repeated php tags

--- admintable/register-view-users.php
+++ admintable/register-view-users.php
@@ -1,41 +1,41 @@
-<?php include('header.php'); ?>
-<?php include('nav.php'); ?>
+<?php include 'header.php';
+include 'nav.php';?>
 <h2>These are the registered users</h2>
 <?php
-	/*This script retrieves all the records from the users table*/
-	require 'mysqli-connect.php'; /*Connect to the database*/
+/*This script retrieves all the records from the users table*/
+require 'mysqli-connect.php'; /*Connect to the database*/
 
-	/*Make the Query*/
-	$q = "SELECT CONCAT(lname, ', ', fname) AS name,
+/*Make the Query*/
+$q = "SELECT CONCAT(lname, ', ', fname) AS name,
 	DATE_FORMAT(registration_date, '%M %D, %Y') AS regdat FROM users
 	ORDER BY registration_date ASC";
-	$result = @mysqli_query($dbcon, $q); //Run the query
+$result = @mysqli_query($dbcon, $q); //Run the query
 
-	if ($result) { //If it ran OK, display thr records
-		/*Table Header*/
-		echo "<table>
+if ($result) {
+	//If it ran OK, display thr records
+	/*Table Header*/
+	echo "<table>
 		<tr>
 		<td><b>Name</b></td>
 		<td><b>Date Registered</b></td>
 		</tr>
 		</table>";
-		/*Fetch and Print all the records*/
+	/*Fetch and Print all the records*/
 
-		while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-			echo '<tr>
+	while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+		echo '<tr>
 			<td>' . $row['name'] . '</td>
 			<td>' . $row['regdat'] . '</td>
-			</tr>' . '<br>' ;
-		}
-		echo '</table>'; //Close the table so that it is ready for displaying.
-		mysqli_free_result($result); //Free up the resources
+			</tr>' . '<br>';
 	}
-	else{
-		/*If if did not run OK*/
-		echo "<p class='alert-box alert round'>The current users could not be retrieved. We apologize for any inconvience.</p>";
+	echo '</table>'; //Close the table so that it is ready for displaying.
+	mysqli_free_result($result); //Free up the resources
+} else {
+	/*If if did not run OK*/
+	echo "<p class='alert-box alert round'>The current users could not be retrieved. We apologize for any inconvience.</p>";
 
-		/*Debug Message*/
-		echo '<p>' . mysqli_error($dbcon) . '<br><br>Query: ' . $q . '</p>';
-	} //End of if ($result)
-	mysqli_close($dbcon); //Close the databse function
+	/*Debug Message*/
+	echo '<p>' . mysqli_error($dbcon) . '<br><br>Query: ' . $q . '</p>';
+}//End of if ($result)
+mysqli_close($dbcon); //Close the databse function
 ?>
