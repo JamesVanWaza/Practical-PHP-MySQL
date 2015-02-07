641da176952c81abce233f56ba8b5ca8005f3107
James Van Waza <jgacuca@gmail.com>
2015-02-06 21:59:42 -0500

    Removed repeated php tags

--- admintable/admin-view-users-page.php
+++ admintable/admin-view-users-page.php
@@ -1,61 +1,61 @@
-<?php include('header.php'); ?>
-<?php include('nav.php'); ?>
-<?php
-	session_start();
-	if (!isset($_SESSION['user_level']) or ($_SESSION['user_level'] !=1)) {
-		header("Location: login.php");
-		exit();
-	}
+<?php include 'header.php';
+include 'nav.php';
+
+session_start();
+if (!isset($_SESSION['user_level']) or ($_SESSION['user_level'] != 1)) {
+	header("Location: login.php");
+	exit();
+}
 ?>
 <h2>These are the registered users</h2>
 <?php
-	/*This script retrieves all the records from the users table*/
-	require 'mysqli-connect.php'; /*Connect to the database*/
+/*This script retrieves all the records from the users table*/
+require 'mysqli-connect.php'; /*Connect to the database*/
 
-	/*Set the number of rows per display page*/
-	$pagerows = 4;
+/*Set the number of rows per display page*/
+$pagerows = 4;
 
-	/*Has the total number of pages already been calculated?*/
-	if (isset($_GET['p']) && is_numeric($_GET['p'])) {//already been calculated
-		$pages = $_GET['p'];
-	}
-	else {
-		/*Use the next block of code to calculate the number of pages*/
-		/*First, check for the total number of records*/
-		$q = "SELECT COUNT(user_id) FROM users";
-		$result = @mysqli_query($dbcon, $q);
-		$row = @mysqli_fetch_array($result, MYSQLI_NUM);
-		$records = $row[0];
+/*Has the total number of pages already been calculated?*/
+if (isset($_GET['p']) && is_numeric($_GET['p'])) {
+//already been calculated
+	$pages = $_GET['p'];
+} else {
+	/*Use the next block of code to calculate the number of pages*/
+	/*First, check for the total number of records*/
+	$q = "SELECT COUNT(user_id) FROM users";
+	$result = @mysqli_query($dbcon, $q);
+	$row = @mysqli_fetch_array($result, MYSQLI_NUM);
+	$records = $row[0];
 
-		/*Now calculate the number of pages*/
-		if ($records > $pagerows) {
-			/*If the number of records will fill more than one page
-			Calculate the number of pages and round the result up to the nearest integer
-			*/
-			$pages = ceil($records / $pagerows);
-		}
-		else {
-			$pages = 1;
-		}
-	}/*Page Check Finished*/
-
-	/*Declare which record to start with*/
-	if (isset($_GET['s']) && is_numeric($_GET['s'])) { /*already been calculated*/
-		$start = $_GET['s'];
+	/*Now calculate the number of pages*/
+	if ($records > $pagerows) {
+		/*If the number of records will fill more than one page
+		Calculate the number of pages and round the result up to the nearest integer
+		 */
+		$pages = ceil($records / $pagerows);
 	} else {
-		$start = 0;
+		$pages = 1;
 	}
+}/*Page Check Finished*/
 
+/*Declare which record to start with*/
+if (isset($_GET['s']) && is_numeric($_GET['s'])) {
+	/*already been calculated*/
+	$start = $_GET['s'];
+} else {
+	$start = 0;
+}
 
-	/*Make the Query*/
-	$q = "SELECT lname, fname, email
+/*Make the Query*/
+$q = "SELECT lname, fname, email
 	DATE_FORMAT(registration_date, '%M %D, %Y') AS regdat FROM users
 	ORDER BY registration_date ASC";
-	$result = @mysqli_query($dbcon, $q); //Run the query
+$result = @mysqli_query($dbcon, $q); //Run the query
 
-	if ($result) { //If it ran OK, display thr records
-		/*Table Header*/
-		echo "<table>
+if ($result) {
+	//If it ran OK, display thr records
+	/*Table Header*/
+	echo "<table>
 		<tr>
 		<td><b>Edit</b></td>
 		<td><b>Delete</b></td>
@@ -65,40 +65,39 @@
 		<td><b>Date Registered</b></td>
 		</tr>
 		</table>";
-		/*Fetch and Print all the records*/
+	/*Fetch and Print all the records*/
 
-		while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-			echo '<tr>
+	while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+		echo '<tr>
 			<td><a href="edit_user.php?id=' . $row['user_id'] . '">Edit</a></td>
 			<td><a href="delete_user.php?id=' . $row['user_id'] . '">Delete</a></td>
 			<td>' . $row['lname'] . '</td>
 			<td>' . $row['fname'] . '</td>
 			<td>' . $row['email'] . '</td>
 			<td>' . $row['regdat'] . '</td>
-			</tr>' . '<br>' ;
-		}
-		echo '</table>'; //Close the table so that it is ready for displaying.
-		mysqli_free_result($result); //Free up the resources
+			</tr>' . '<br>';
 	}
-	else{
-		/*If if did not run OK*/
-		echo "<p class='alert-box alert round'>The current users could not be retrieved. We apologize for any inconvience.</p>";
+	echo '</table>'; //Close the table so that it is ready for displaying.
+	mysqli_free_result($result); //Free up the resources
+} else {
+	/*If if did not run OK*/
+	echo "<p class='alert-box alert round'>The current users could not be retrieved. We apologize for any inconvience.</p>";
 
-		/*Debug Message*/
-		echo '<p>' . mysqli_error($dbcon) . '<br><br>Query: ' . $q . '</p>';
-	} //End of if ($result)
-	/*Now display the figure for the total number of records/members*/
-	$q  = "SELECT COUNT(user_id) FROM users";
-	$result = @mysqli_query($dbcon, $q);
-	$row = @mysqli_fetch_array($result, MYSQLI_NUM);
-	$members = $row[0];
-	mysqli_close($dbcon); //Close the databse function
-	echo "<p>Total membership: $members</p>";
-	if ($pages > 1) {
-		echo '<p>';
+	/*Debug Message*/
+	echo '<p>' . mysqli_error($dbcon) . '<br><br>Query: ' . $q . '</p>';
+}//End of if ($result)
+/*Now display the figure for the total number of records/members*/
+$q = "SELECT COUNT(user_id) FROM users";
+$result = @mysqli_query($dbcon, $q);
+$row = @mysqli_fetch_array($result, MYSQLI_NUM);
+$members = $row[0];
+mysqli_close($dbcon); //Close the databse function
+echo "<p>Total membership: $members</p>";
+if ($pages > 1) {
+	echo '<p>';
 
 	/*What number is the current page?*/
-	$current_page = ($start/$pagerows) + 1;
+	$current_page = ($start / $pagerows) + 1;
 
 	/*If the page is not the first page then create a Previous Link*/
 	if ($current_page != 1) {
@@ -110,5 +109,5 @@
 		echo '<a href="register-view-users.php?s=' . ($start + $pagerows) . '&p=' . $pages . '">Next</a>';
 	}
 	echo "</p>";
-	}
+}
 ?>
