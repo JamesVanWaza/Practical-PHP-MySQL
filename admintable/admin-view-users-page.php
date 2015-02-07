b09dc0091ac75fe9239fd68036263413f9f6972e
James Van Waza <jgacuca@gmail.com>
2015-02-06 22:45:10 -0500

    Changed the Select Query to Admintable

--- admintable/admin-view-users-page.php
+++ admintable/admin-view-users-page.php
@@ -1,113 +1,100 @@
-<?php include 'header.php';
-include 'nav.php';
-
+<?php
 session_start();
 if (!isset($_SESSION['user_level']) or ($_SESSION['user_level'] != 1)) {
 	header("Location: login.php");
 	exit();
 }
 ?>
-<h2>These are the registered users</h2>
+<h2 class="text-center">Registered members displayed four at a time</h2>
 <?php
-/*This script retrieves all the records from the users table*/
-require 'mysqli-connect.php'; /*Connect to the database*/
-
-/*Set the number of rows per display page*/
-$pagerows = 4;
-
-/*Has the total number of pages already been calculated?*/
-if (isset($_GET['p']) && is_numeric($_GET['p'])) {
+// This code retrieves all the records from the users table.
+require 'mysqli-connect.php'; // Connect to the database.
+//set the number of rows per display page
+$pagerows = 4; #3
+// Has the total number of pages already been calculated?
+if (isset($_GET['p']) && is_numeric
+	($_GET['p'])) {
 //already been calculated
 	$pages = $_GET['p'];
 } else {
-	/*Use the next block of code to calculate the number of pages*/
-	/*First, check for the total number of records*/
+//use the next block of code to calculate the number of pages
+	//First, check for the total number of records
 	$q = "SELECT COUNT(user_id) FROM users";
 	$result = @mysqli_query($dbcon, $q);
 	$row = @mysqli_fetch_array($result, MYSQLI_NUM);
 	$records = $row[0];
-
-	/*Now calculate the number of pages*/
-	if ($records > $pagerows) {
-		/*If the number of records will fill more than one page
-		Calculate the number of pages and round the result up to the nearest integer
-		 */
-		$pages = ceil($records / $pagerows);
+//Now calculate the number of pages
+	if ($records > $pagerows) {//if the number of records will fill more than one page
+		//Calculatethe number of pages and round the result up to the nearest integer
+		$pages = ceil($records / $pagerows); #5
 	} else {
 		$pages = 1;
 	}
-}/*Page Check Finished*/
-
-/*Declare which record to start with*/
-if (isset($_GET['s']) && is_numeric($_GET['s'])) {
-	/*already been calculated*/
+}//page check finished
+//Declare which record to start with
+if (isset($_GET['s']) && is_numeric
+	($_GET['s'])) {
+//already been calculated
 	$start = $_GET['s'];
 } else {
 	$start = 0;
 }
-
-/*Make the Query*/
-$q = "SELECT lname, fname, email
-	DATE_FORMAT(registration_date, '%M %D, %Y') AS regdat FROM users
-	ORDER BY registration_date ASC";
-$result = @mysqli_query($dbcon, $q); //Run the query
-
+// Make the query:
+$q = "SELECT lname, fname, email, DATE_FORMAT(registration_date, '%M %d, %Y')
+AS regdat, user_id FROM admintable ORDER BY registration_date ASC LIMIT $start, $pagerows";
+$result = @mysqli_query($dbcon, $q); // Run the query.
+$members = mysqli_num_rows($result);
 if ($result) {
-	//If it ran OK, display thr records
-	/*Table Header*/
-	echo "<table>
-		<tr>
-		<td><b>Edit</b></td>
-		<td><b>Delete</b></td>
-		<td><b>Last Name</b></td>
-		<td><b>First Name</b></td>
-		<td><b>Email</b></td>
-		<td><b>Date Registered</b></td>
-		</tr>
-		</table>";
-	/*Fetch and Print all the records*/
-
+	// If it ran OK, display the records.
+	// Table header.
+	echo '<table>
+<tr><td><b>Edit</b></td>
+<td><b>Delete</b></td>
+<td><b>Last Name</b></td>
+<td><b>First Name</b></td>
+<td><b>Email</b></td>
+<td><b>Date Registered</b></td>
+</tr>';
+// Fetch and print the records:
 	while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
 		echo '<tr>
-			<td><a href="edit_user.php?id=' . $row['user_id'] . '">Edit</a></td>
-			<td><a href="delete_user.php?id=' . $row['user_id'] . '">Delete</a></td>
-			<td>' . $row['lname'] . '</td>
-			<td>' . $row['fname'] . '</td>
-			<td>' . $row['email'] . '</td>
-			<td>' . $row['regdat'] . '</td>
-			</tr>' . '<br>';
+<td><a href="edit_record.php?id=' . $row['user_id'] . '">Edit</a></td>
+<td><a href="delete_record.php?id=' . $row['user_id'] . '">Delete</a></td>
+<td>' . $row['lname'] . '</td>
+<td>' . $row['fname'] . '</td>
+<td>' . $row['email'] . '</td>
+<td>' . $row['regdat'] . '</td>
+</tr>';
 	}
-	echo '</table>'; //Close the table so that it is ready for displaying.
-	mysqli_free_result($result); //Free up the resources
+	echo '</table>'; // Close the table
+	mysqli_free_result($result); // Free up the resources
 } else {
-	/*If if did not run OK*/
+	// If it failed to run
+	// Error message
 	echo "<p class='alert-box alert round'>The current users could not be retrieved. We apologize for any inconvience.</p>";
-
-	/*Debug Message*/
-	echo '<p>' . mysqli_error($dbcon) . '<br><br>Query: ' . $q . '</p>';
-}//End of if ($result)
-/*Now display the figure for the total number of records/members*/
+	// Debug message
+	echo '<p>' . mysqli_error($dbcon) . '<br><br />Query: ' . $q . '</p>';
+}// End of if ($result)
+//Now display the figure for the total number of records/members
 $q = "SELECT COUNT(user_id) FROM users";
 $result = @mysqli_query($dbcon, $q);
 $row = @mysqli_fetch_array($result, MYSQLI_NUM);
 $members = $row[0];
-mysqli_close($dbcon); //Close the databse function
+mysqli_close($dbcon); // Close the database connection
 echo "<p>Total membership: $members</p>";
 if ($pages > 1) {
+	#10
 	echo '<p>';
-
-	/*What number is the current page?*/
+//What number is the current page?
 	$current_page = ($start / $pagerows) + 1;
-
-	/*If the page is not the first page then create a Previous Link*/
+//If the page is not the first page then create a Previous link
 	if ($current_page != 1) {
-		echo '<a href="register-view-users.php?s=' . ($start - $pagerows) . '&p=' . $pages . '">Previous</a>';
+		echo '<a href="register-view_users.php?s=' . ($start - $pagerows) . '&p=' . $pages . '">Previous</a> ';
 	}
-
-	/*Create a Next link*/
+//Create a Next link
 	if ($current_page != $pages) {
-		echo '<a href="register-view-users.php?s=' . ($start + $pagerows) . '&p=' . $pages . '">Next</a>';
+		echo '<a href="register-view_users.php?s=' . ($start + $pagerows) . '&p=' . $pages . '">Next</a> ';
 	}
-	echo "</p>";
+	echo '</p>';
 }
 ?>
