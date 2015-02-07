b5b55dc58516390c8786749cd0a108d4d86ca351
James Van Waza <jgacuca@gmail.com>
2015-02-07 15:35:08 -0500

    Added the default session start at the top of the search page

--- admintable/search.php
+++ admintable/search.php
@@ -1,11 +1,12 @@
-<?php include('header.php'); ?>
-<?php include('nav.php'); ?>
 <?php
-	session_start();
-	if (!isset($_SESSION['user_level']) or ($_SESSION['user_level'] !=1)) {
-		header("Location: login.php");
-		exit();
-	}
+ob_start();
+session_start();
+if (!isset($_SESSION['user_level']) or ($_SESSION['user_level'] != 1)) {
+	header("Location: login.php");
+	exit();
+}
+include 'header-admin.php';
+ob_end_flush();
 ?>
 <h3>Search for a Record</h3>
 <!--display the form on the screen-->
@@ -13,12 +14,18 @@
   <div class="row"><!--Beginning of First Row-->
   	<div class="large-6 medium-6 small-12 columns">
       <label>First Name
-        <input id="fname" type="text" name="fname" size="30" maxlength="30" placeholder="First Name" value="<?php if (isset($_POST['fname'])) echo $_POST['fname']; ?>"/>
+        <input id="fname" type="text" name="fname" size="30" maxlength="30" placeholder="First Name" value="<?php if (isset($_POST['fname'])) {
+	echo $_POST['fname'];
+}
+?>"/>
       </label>
     </div>
      <div class="large-6 medium-6 small-12 columns">
       <label>Last Name
-        <input id="lname" type="text" name="lname" size="40" maxlength="40" placeholder="Last Name" value="<?php if (isset($_POST['lname'])) echo $_POST['lname']; ?>"/>
+        <input id="lname" type="text" name="lname" size="40" maxlength="40" placeholder="Last Name" value="<?php if (isset($_POST['lname'])) {
+	echo $_POST['lname'];
+}
+?>"/>
       </label>
     </div>
   </div><!--End of First Row-->
