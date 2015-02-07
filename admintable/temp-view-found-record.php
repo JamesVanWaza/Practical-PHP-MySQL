c8600f4311bdd56784d57efc0f9df4aa29a14452
James Van Waza <jgacuca@gmail.com>
2015-02-07 15:26:42 -0500

    Revision of the search page - It works

--- admintable/temp-view-found-record.php
+++ admintable/temp-view-found-record.php
@@ -8,59 +8,51 @@ if (!isset($_SESSION['user_level']) or ($_SESSION['user_level'] != 1)) {
 include 'header-admin.php';
 ob_end_flush();
 ?>
-<h2 class="text-center">Search Results</h2>
+<h2 class="text-center">Search Result</h2>
 <?php
-/*This script retrieves all the records from the users table*/
-require 'mysqli-connect.php'; /*Connect to the database*/
-
-//echo '<p>If no record is shown, this is because you had an incorrect or missing entry in the search form.<br>Click the back button on the browser and try again</p>';
-//$fname = $_POST['fname'];
-//$lname = $_POST['lname'];
-
-/*Make the Query Using Hard Coded Names*/
-$q = "SELECT lname, fname, email, DATE_FORMAT(registration_date, '%M %D, %Y') AS regdat
-		FROM admintable WHERE lname='Smith' AND fname='James'
-		ORDER BY registration_date ASC";
-$result = @mysqli_query($dbcon, $q); //Run the query
-
+// This code retrieves particular records from the users table
+require 'mysqli-connect.php'; // Connect to the database
+/*The query retrieves all the James Smith in the database*/
+$q = "SELECT lname, fname, email, DATE_FORMAT(registration_date, '%M %d, %Y') AS regdat, user_id
+FROM admintable
+WHERE lname='Smith'AND fname='James'
+ORDER BY registration_date ASC ";
+$result = @mysqli_query($dbcon, $q); // Run the query
 if ($result) {
-	//If it ran OK, display the records
-	/*Table Header*/
-	echo "<table>
-		<tr>
-		<td><b>Edit</b></td>
-		<td><b>Delete</b></td>
-		<td><b>Last Name</b></td>
-		<td><b>First Name</b></td>
-		<td><b>Email</b></td>
-		<td><b>Date Registered</b></td>
-		</tr>
-		</table>";
-	/*Fetch and Print all the records*/
-
+	// If it ran, display the records
+	// Display the table headings
+	echo '<table>
+<tr><td><b>Edit</b></td>
+<td><b>Delete</b></td>
+<td><b>Last Name</b></td>
+<td><b>First Name</b></td>
+<td><b>Email</b></td>
+<td><b>Date Registered</b></td>
+</tr>';
+// Fetch and display the records
 	while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
 		echo '<tr>
-			<td><a href="edit-user.php?id=' . $row['user_id'] . ' ">Edit</a></td>
-			<td><a href="delete-user.php?id=' . $row['user_id'] . ' ">Delete</a></td>
-			<td>' . $row['lname'] . '</td>
-			<td>' . $row['fname'] . '</td>
-			<td>' . $row['email'] . '</td>
-			<td>' . $row['regdat'] . '</td>
-			</tr>' . '<br>';
+    <td><a href="edit-user.php?id=' . $row['user_id'] . '">Edit</a></td>
+    <td><a href="delete-user.php?id=' . $row['user_id'] . '">Delete</a></td>
+    <td>' . $row['lname'] . '</td>
+    <td>' . $row['fname'] . '</td>
+    <td>' . $row['email'] . '</td>
+    <td>' . $row['regdat'] . '</td>
+    </tr>';
 	}
-	echo '</table>'; //Close the table so that it is ready for displaying.
-	mysqli_free_result($result); //Free up the resources
+	echo '</table>'; // Close the table
+	mysqli_free_result($result); // Free up the resources
 } else {
-	/*If if did not run OK*/
-	echo "<p class='alert-box alert round'>The current users could not be retrieved. We apologize for any inconvience.</p>";
-
-	/*Debug Message*/
+	// If it did not run properly
+	// Message
+	echo '<p class="alert-box alert round">The current users could not be retrieved. We apologize for any inconvenience.</p>';
+// Debugging message
 	echo '<p>' . mysqli_error($dbcon) . '<br><br>Query: ' . $q . '</p>';
-}//End of if ($result)
-$q = "SELECT COUNT(user_id) FROM admintable";
-$result = @mysqli_query($dbcon, $q); //Run the query
-$row = $mysqli_fetch_array($result, MYSQLI_NUM);
+}// End of if ($result). Now display the figure for total number of records/members
+$q = "SELECT COUNT(user_id) FROM users";
+$result = @mysqli_query($dbcon, $q);
+$row = @mysqli_fetch_array($result, MYSQLI_NUM);
 $members = $row[0];
-mysqli_close($dbcon); //Close the databse function
-echo "<p>" . "Total Membership: $members" . "</p>";
+mysqli_close($dbcon); // Close the database connection
+echo "<p>Total membership: $members</p>";
 ?>
