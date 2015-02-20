3eb30eee5f9efb44b44757ad6d55b7f14b6a69ce
James Van Waza <jgacuca@gmail.com>
2015-02-20 06:21:10 -0500

    Changed the select statement and added the missing td tags in the table

--- postalDB/view-found-record.php
+++ postalDB/view-found-record.php
@@ -19,7 +19,7 @@ $lname = $_POST['lname'];
 
 /*Make the Query Using Hard Coded Names*/
 $q = "SELECT lname, fname, email, DATE_FORMAT(registration_date, '%M %D, %Y')
-			AS regdat FROM postaldb WHERE lname='$lname' AND fname='$fname'
+			AS regdat, class, paid, user_id FROM postaldb WHERE lname='$lname' AND fname='$fname'
 			ORDER BY registration_date ASC";
 $result = @mysqli_query($dbcon, $q); //Run the query
 
@@ -39,12 +39,14 @@ if ($result) {
 
 	while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
 		echo '<tr>
-			<td><a href="edit_user.php?id=' . $row['user_id'] . ' ">Edit</a></td>
-			<td><a href="delete_user.php?id=' . $row['user_id'] . ' ">Delete</a></td>
+			<td><a href="edit-user.php?id=' . $row['user_id'] . ' ">Edit</a></td>
+			<td><a href="delete-user.php?id=' . $row['user_id'] . ' ">Delete</a></td>
 			<td>' . $row['lname'] . '</td>
 			<td>' . $row['fname'] . '</td>
 			<td>' . $row['email'] . '</td>
 			<td>' . $row['regdat'] . '</td>
+			<td>' . $row['class'] . '</td>
+			<td>' . $row['paid'] . '</td>
 			</tr>' . '<br>';
 	}
 	echo '</table>'; //Close the table so that it is ready for displaying.
