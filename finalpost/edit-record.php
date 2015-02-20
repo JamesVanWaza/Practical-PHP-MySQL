d3b2d9af66454b1f0b7b704e7759c4895e07bfd6
James Van Waza <jgacuca@gmail.com>
2015-02-20 18:55:39 -0500

    Fixed the SQL Statement

--- finalpost/edit-record.php
+++ finalpost/edit-record.php
@@ -3,90 +3,107 @@ ob_start();
 session_start();
 if (!isset($_SESSION['user_level']) or ($_SESSION['user_level'] != 1)) {
 	header("Location: login.php");
+	include 'header-members.php';
 	exit();
 }
-include 'header-members.php';
 ob_end_flush();
 ?>
 <h2>Edit a Record</h2>
 <?php
-/*After clicking the Edit Link found_record.php page, the editing interface appears*/
-/*The code looks for a valid user ID, either through GET or POST*/
+// After clicking the Edit link in the register_found_record.php page. This editing interface appears
+// Look for a valid user ID, either through GET or POST:
 if ((isset($_GET['id'])) && (is_numeric($_GET['id']))) {
-//From view-users.php
+	// From view-users.php
 	$id = $_GET['id'];
 } elseif ((isset($_POST['id'])) && (is_numeric($_POST['id']))) {
-	/*Form submission*/
+	// Form submission.
 	$id = $_POST['id'];
 } else {
+	// No valid ID? stop the script.
 	echo "<p class='alert-box alert round'>This page has been accessed in error</p>";
 	include 'footer.php';
 	exit();
 }
 require 'mysqli-connect.php';
-/*Has the form been submitted?*/
+// Has the form been submitted?
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$errors = array();
-
-	/*Look for the first name*/
+	// Look for the first name:
 	if (empty($_POST['fname'])) {
 		$errors[] = 'You forgot to enter the first name.';
 	} else {
 		$fn = mysqli_real_escape_string($dbcon, trim($_POST['fname']));
 	}
-
-	/*Look for the last name*/
+	// Look for the last name:
 	if (empty($_POST['lname'])) {
-		$errors[] = 'You forgot to enter your last name.';
+		$errors[] = 'You forgot to enter the last name.';
 	} else {
 		$ln = mysqli_real_escape_string($dbcon, trim($_POST['lname']));
 	}
-
-	/*Look for the email address*/
+	// Look for the email address:
 	if (empty($_POST['email'])) {
-		$errors[] = 'You forgot to enter your email address.';
+		$errors[] = 'You forgot to enter the email address.';
 	} else {
 		$e = mysqli_real_escape_string($dbcon, trim($_POST['email']));
 	}
-	if (empty($errors)) {
-		/*If everything is ok, make the update query
-		Check that the email is not already in the users table
-		 */
-		$q = "UPDATE finalpost SET fname='$fn', lname='$ln', email='$e' WHERE user_id=$id LIMIT 1";
-		$result = @mysqli_query($dbcon, $q); // Run the query.
-		if (mysqli_affected_rows($dbcon) == 1) {
-			/*If it ran OK Echo a message if the edit was satisfactory*/
-			echo "<h3>The user has been edited</h3>";
-		} else {
-			/*Echo a message if the query failed*/
-			echo '<p class="alert-box alert round">The user could not be edited due to a system error<br>We apoligize for any inconvience</p>'; /*Error message*/
-			echo "<p>" . mysqli_error($dbcon) . '<br>Query: ' . $q . "</p>"; //Debugging message
-		}
-
+	// Look for the class of membership:
+	if (!empty($_POST['class'])) {
+		$class = mysqli_real_escape_string($dbcon, trim($_POST['class']));
 	} else {
-		/*If the email address is already registered*/
-		echo '<p class="alert-box alert round">The email address has already been registered</p>'; /*Error message*/
+		$class = NULL;
 	}
-} else {
-	//Display the errors
-	echo '<p class="alert-box alert round">The following errors occured:<br>';
-	foreach ($errors as $msg) {
-		/*Echo each error*/
-		echo "- $msg<br>\n";
+	// Look for the payment confirmation:
+	if (!empty($_POST['paid'])) {
+		$paid = mysqli_real_escape_string($dbcon, trim($_POST['paid']));
+	} else {
+		$paid = NULL;
 	}
-	echo '</p><p>Please try again</p>'; /*Error message*/
-	echo "<p>" . mysqli_error($dbcon) . '<br>Query: ' . $q . "</p>"; //Debugging message
-}//End of if(empty($errors)) section
-
-//Select the record
-$q = "SELECT fname, lname, email FROM finalpost WHERE user_id=$id";
+	if (empty($errors)) {
+		// If no problems occurred
+		//determine whether the email address has already been registered for a user,
+		//but ignore the email address of the user being updated, he may wish to keep his
+		//current email address
+		$q = "SELECT user_id FROM finalpost WHERE email = '$e' AND user_id != $id";
+		$result = @mysqli_query($dbcon, $q);
+		if (mysqli_num_rows($result) == 0) {
+//The email address is not already registered or it
+			//belongs to the user being updated, therefore, update the finalpost table
+			$q = "UPDATE finalpost SET fname='$fn', lname='$ln', email='$e', class='$class',
+paid='$paid' WHERE user_id=$id LIMIT 1";
+			$result = @mysqli_query($dbcon, $q);
+			if (mysqli_affected_rows($dbcon) == 1) {
+				// If it ran OK
+				// Echo a message confirming that the edit was satisfactory
+				echo '<h3>The user has been edited.</h3>';
+			} else {
+				// Echo a message if the query failed
+				echo '<p class="alert-box alert round">The user could not be edited due to a system error<br>We apoligize for any inconvience</p>';
+// Error message
+				echo '<p>' . mysqli_error($dbcon) . '<br />Query: ' . $q . '</p>';
+// Debugging message
+			}
+		} else {
+//The email address is already registered for another user
+			echo '<p class="alert-box alert round">The email address has already been registered</p>';
+		}
+	} else {
+		// Display the errors
+		echo '<p class="alert-box alert round">The following errors occured:<br>';
+		foreach ($errors as $msg) {
+			// Echo each error.
+			echo " - $msg<br />\n";
+		}
+		echo '</p><p>Please try again.</p>';
+	}// End of if (empty($errors))section.
+}// End of the conditionals
+// Select the user's information:
+$q = "SELECT fname, lname, email, class, paid FROM finalpost WHERE user_id=$id";
 $result = @mysqli_query($dbcon, $q);
 if (mysqli_num_rows($result) == 1) {
-	/*Valid user ID, display the form
-	and get the user's information
-	 */
+	// Valid user ID, display the form.
+	// Get the user's information:
 	$row = mysqli_fetch_array($result, MYSQLI_NUM);
-	/*Create the Form*/
+	// Create the form:
 	echo '<form action="edit-record.php" method="post">
   <div class="row">
     <div class="large-6 medium-6 small-12 columns">
@@ -113,7 +130,7 @@ if (mysqli_num_rows($result) == 1) {
     </div><!--End of Third Row-->
 </form>';
 } else {
-	/*The record could not be validated*/
+	// The user could not be validated
 	echo '<p class="alert-box alert round">This page has been accessed in error';
 }
 mysqli_close($dbcon);
