981ea7c86ccd2cbd6779f67cd197dd57bee4e7c1
James Van Waza <jgacuca@gmail.com>
2015-02-20 22:20:41 -0500

    Showing the Admin View Users Page

--- lmnmigrate/admin-view-users.php
+++ lmnmigrate/admin-view-users.php
@@ -8,44 +8,94 @@ if (!isset($_SESSION['user_level']) or ($_SESSION['user_level'] != 1)) {
 include 'nav.php';
 ob_end_flush();
 ?>
-<h2>These are the registered users</h2>
+<h2>Registered members displayed four at-a-time</h2>
 <?php
-	/*This script retrieves all the records from the logindb table*/
-	require ('mysqli-connect.php'); /*Connect to the database*/
-
-	/*Make the Query*/
-	$q = "SELECT CONCAT(lname, ', ', fname) AS name, DATE_FORMAT(registration_date, '%M %D, %Y') AS regdat
-	FROM logindb
-	ORDER BY registration_date ASC";
-	$result = @mysqli_query($dbcon, $q); //Run the query
-
-	if ($result) { //If it ran OK, display thr records
-		/*Table Header*/
-		echo "<table>
-		<tr>
-		<td><b>Edit</b></td>
-		<td><b>Delete</b></td>
-		<td><b>Name</b></td>
-		<td><b>Date Registered</b></td>
-		</tr>
-		";
-		/*Fetch and Print all the records*/
-
-		while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-			echo '<tr>
-			<td>' . $row['name'] . '</td>
-			<td>' . $row['regdat'] . '</td>
-			</tr>' . '<br>' ;
-		}
-		echo '</table>'; //Close the table so that it is ready for displaying.
-		mysqli_free_result($result); //Free up the resources
+// This script retrieves all the records from the users table
+require 'mysqli-connect.php'; // Connect to the database
+//set the number of rows per display page
+$pagerows = 4;
+// Has the total number of pagess already been calculated?
+if (isset($_GET['p']) && is_numeric
+	($_GET['p'])) {
+//already been calculated
+	$pages = $_GET['p'];
+} else {
+//use the next block of code to calculate the number of pages
+	//First, check for the total number of records
+	$q = "SELECT COUNT(user_id) FROM lmnmigrate";
+	$result = @mysqli_query($dbcon, $q);
+	$row = @mysqli_fetch_array($result, MYSQLI_NUM);
+	$records = $row[0];
+//Now calculate the number of pages
+	if ($records > $pagerows) {
+		//if the number of records will fill more than one page
+		//Calculate the number of pages and round the result up to the nearest integer
+		$pages = ceil($records / $pagerows);
+	} else {
+		$pages = 1;
 	}
-	else{
-		/*If if did not run OK*/
-		echo "<p class='alert-box alert round'>The current users could not be retrieved. We apologize for any inconvience.</p>";
-
-		/*Debug Message*/
-		echo '<p>' . mysqli_error($dbcon) . '<br><br>Query: ' . $q . '</p>';
-	} //End of if ($result)
-	mysqli_close($dbcon); //Close the databse function
-?>
+}//page check finished
+//Decalre which record to start with
+if (isset($_GET['s']) && is_numeric
+	($_GET['s'])) {
+//already been calculated
+	$start = $_GET['s'];
+} else {
+	$start = 0;
+}
+// Make the query:
+$q = "SELECT lname, fname, email, DATE_FORMAT(registration_date, '%M %d, %Y') AS regdat, user_id FROM lmnmigrate ORDER BY registration_date ASC LIMIT $start, $pagerows";
+$result = @mysqli_query($dbcon, $q); // Run the query
+$members = mysqli_num_rows($result);
+if ($result) {
+	// If it ran OK, display the records
+	// Table header
+	echo '<table>
+<tr><td><b>Edit</b></td>
+<td><b>Delete</b></td>
+<td><b>Last Name</b></td>
+<td><b>First Name</b></td>
+<td><b>Email</b></td>
+<td><b>Date Registered</b></td>
+</tr>';
+// Fetch and print all the records
+	while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+		echo '<tr>
+	<td><a href="edit-record.php?id=' . $row['user_id'] . '">Edit</a></td>
+	<td><a href="delete-record.php?id=' . $row['user_id'] . '">Delete</a></td>
+	<td>' . $row['lname'] . '</td>
+	<td>' . $row['fname'] . '</td>
+	<td>' . $row['email'] . '</td>
+	<td>' . $row['regdat'] . '</td>
+	</tr>';
+	}
+	echo '</table>'; // Close the table
+	mysqli_free_result($result); // Free up the resources
+} else {
+	// If it did not run OK.
+	// Message:
+	echo "<p class='alert-box alert round'>The current users could not be retrieved. We apologize for any inconvience.</p>";
+	// Debugging message
+	echo '<p>' . mysqli_error($dbcon) . '<br><br>Query: ' . $q . '</p>';
+}// End of if ($result). Now display the total number of records/members
+$q = "SELECT COUNT(user_id) FROM lmnmigrate";
+$result = @mysqli_query($dbcon, $q);
+$row = @mysqli_fetch_array($result, MYSQLI_NUM);
+$members = $row[0];
+mysqli_close($dbcon); // Close the database connection
+echo "<p>Total membership: $members</p>";
+if ($pages > 1) {
+	echo '<p>';
+//What number is the current page?
+	$current_page = ($start / $pagerows) + 1;
+//If the page is not the first page then create a Previous link
+	if ($current_page != 1) {
+		echo '<a href="admin-view-users.php?s=' . ($start - $pagerows) . '&p=' . $pages . '">Previous</a> ';
+	}
+//Create a Next link
+	if ($current_page != $pages) {
+		echo '<a href="admin-view-users.php?s=' . ($start + $pagerows) . '&p=' . $pages . '">Next</a> ';
+	}
+	echo '</p>';
+}
+include "footer.php";?>
