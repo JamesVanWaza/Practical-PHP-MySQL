b2009acbd2f4d43e84a3efee5e8f56ec3f8106a8
James Van Waza <jgacuca@gmail.com>
2015-02-21 14:38:55 -0500

    Only one html5req file will be used

--- html5req.php
+++ html5req.php
@@ -2,15 +2,15 @@
 <html lang="en">
 <meta charset="utf-8">
 <head>
-    <link href="simpleidb/css/foundation.css" rel="stylesheet" type="text/css">
+    <link href="css/foundation.css" rel="stylesheet" type="text/css">
     <link href='http://fonts.googleapis.com/css?family=Poiret+One|Quattrocento:400,700|Quattrocento+Sans:400,400italic|Raleway:400,500' rel='stylesheet' type='text/css'>
-    <link href="simpleidb/css/main.css" rel="stylesheet" type="text/css">
+    <link href="css/main.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-    <script src="simpleidb/js/vendor/jquery.js"></script>
-    <script src="simpleidb/js/vendor/modernizr.js"></script>
-    <script src="simpleidb/js/vendor/fastclick.js"></script>
-    <script src="simpleidb/js/foundation.min.js"></script>
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/modernizr.js"></script>
+    <script src="js/vendor/fastclick.js"></script>
+    <script src="js/foundation.min.js"></script>
     <script>
         $(document).foundation();
     </script>
