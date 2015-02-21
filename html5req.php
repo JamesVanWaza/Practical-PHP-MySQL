3e8e8e31f326a30b3a02842ea19ec17003f6b65e
James Van Waza <jgacuca@gmail.com>
2015-02-21 14:36:48 -0500

    Only one html5 req file will be used

---
+++ html5req.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8">
+<head>
+    <link href="simpleidb/css/foundation.css" rel="stylesheet" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Poiret+One|Quattrocento:400,700|Quattrocento+Sans:400,400italic|Raleway:400,500' rel='stylesheet' type='text/css'>
+    <link href="simpleidb/css/main.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <script src="simpleidb/js/vendor/jquery.js"></script>
+    <script src="simpleidb/js/vendor/modernizr.js"></script>
+    <script src="simpleidb/js/vendor/fastclick.js"></script>
+    <script src="simpleidb/js/foundation.min.js"></script>
+    <script>
+        $(document).foundation();
+    </script>
+</body>
+</html>
--- simpleIDB/register-thanks.php
+++ simpleIDB/register-thanks.php
@@ -1,2 +1,2 @@
-<?php include 'html5req.php'; ?>
+<?php include '../html5req.php'; ?>
 <h2>Thanks! Your Registration has been submitted succesffully.</h2>
