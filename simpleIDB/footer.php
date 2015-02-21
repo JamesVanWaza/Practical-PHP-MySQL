e2d676ee5b2c47fdd12bad957d3a1017ed7879ad
James Van Waza <jgacuca@gmail.com>
2015-02-21 09:55:21 -0500

    Changed the copyright to show the year

--- simpleIDB/footer.php
+++ simpleIDB/footer.php
@@ -1,9 +1,4 @@
 <footer>
-	<p>Copyright &copy; Adrian West 2012  Designed by
-	<br>
-	<a"http://www.colycomputerhelp.co.uk/">Adrian West </a>
-	<br>
-	<a"http://jigsaw.w3.org/css-validator/">CSS</a>
-	<br>
-	<a"http://validator.w3.org/">HTML5</a></p>
+	&copy; <?php /*Copyright of the Year*/echo date("Y") . "<br>";?>
+	<!--&copy; 2015 - --> <?//php echo date("Y");?>
 </footer>
