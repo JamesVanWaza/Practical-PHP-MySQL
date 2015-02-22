82316930215e0366506add1bb30f4eb7794a4382
James Van Waza <jgacuca@gmail.com>
2015-02-22 16:02:11 -0500

    Added extra fonts in the html5req page

--- html5req.php
+++ html5req.php
@@ -3,7 +3,7 @@
 <meta charset="utf-8">
 <head>
     <link href="css/foundation.css" rel="stylesheet" type="text/css">
-    <link href='http://fonts.googleapis.com/css?family=Poiret+One|Quattrocento:400,700|Quattrocento+Sans:400,400italic|Raleway:400,500' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Poiret+One|Quattrocento:400,700|Quattrocento+Sans:400,400italic|Raleway:400,500|Maven+Pro|Cambay' rel='stylesheet' type='text/css'>
     <link href="css/main.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 </head>
