06c31ebdd738271aa2ac4759fdaef2cdc41fc278
jgacuca567 <jgacuca@gmail.com>
2015-04-29 14:15:36 +0000

    Added the UNIX Timestamp Function

--- PHPMYSQLBTB/btb_sandbox/date_time_unix.php
+++ PHPMYSQLBTB/btb_sandbox/date_time_unix.php
@@ -1,8 +1,7 @@
 <h1>Dates and Times: Unix</h1>
 <?php
+    include_once('html5req.php');
+    date_default_timezone_set('America/New_York');
     //Getting the Unix Timestamp
-    echo time();
-
-      //Getting the Unix Timestamp
-    echo time();
+    echo "Current timestamp: " . time() . "<br>";
 ?>
