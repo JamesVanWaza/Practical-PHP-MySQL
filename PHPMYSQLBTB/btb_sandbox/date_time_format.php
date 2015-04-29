6a74237f19ccc57132c5552d515c211f9f4f917c
jgacuca567 <jgacuca@gmail.com>
2015-04-29 14:34:47 +0000

    Code cleanup of the text elements

--- PHPMYSQLBTB/btb_sandbox/date_time_format.php
+++ PHPMYSQLBTB/btb_sandbox/date_time_format.php
@@ -1,4 +1,8 @@
+<h1>Date Time Formatting</h1>
 <?php
+//See http://php.net/manual/en/function.strftime.php
 include_once('html5req.php');
-
+date_default_timezone_set('America/New_York');
+$timestamp = time();
+echo strftime("The Date Today is %m/%d/%y");
 ?>
