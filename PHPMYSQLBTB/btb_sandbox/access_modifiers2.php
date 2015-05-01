ed6b2dc8656198615014907f755ba72bec1d87b0
jgacuca567 <jgacuca@gmail.com>
2015-05-01 15:27:46 +0000

    Added More examples of the access_modifiers

--- PHPMYSQLBTB/btb_sandbox/access_modifiers2.php
+++ PHPMYSQLBTB/btb_sandbox/access_modifiers2.php
@@ -1,55 +1,19 @@
 <?php
-    class Example {
-        public $a = 1;
-        protected $b = 2;
-        private $c = 3;
+    class SetterGetterExample {
+        private $a = 1;
         
-        function show_abc(){
-            echo $this->a;
-            echo $this->b;
-            echo $this->c;
+        public function get_a(){
+            return $this->a;
         }
         
-        public function hello_everyone(){
-            return "Hello Everyone";
+        public function set_a($value){
+            $this->a =$value;
         }
-        
-        protected function hello_family(){
-            return "Hello family";
-        }
-        
-        private function hello_me(){
-            return "Hello me";
-        }
-        
-        //public by default
-        function hello(){
-            $output = $this->hello_everyone();
-            $output .= $this->hello_family();
-            $output .= $this->hello_me();
-            return $output;
-        }
-             
     }
+$example = new SetterGetterExample();
+// restricted : echo $example->a . '<br>';
 
-$example = new Example();
-echo "public a: {$example->a}<br>";
-
-//Will Return an error by default
-//echo "protected b: {$example->b}<br>";
-
-//Will Return an error by default
-//echo "private c: {$example->c}<br>";
-$example->show_abc();
-echo '<br>';
-echo "hello everyone: {$example->hello_everyone()}<br>";
-echo '<br>';
-//Returns an error by default
-//echo "hello family: {$example->hello_family()}<br>";
-
-//Returns an error by default
-//echo "hello me: {$example->hello_me()}<br>";
-//
-
-echo $example->hello() . '<br>';
+echo $example->get_a() . '<br>';
+$example->set_a(15);
+echo $example->get_a() . '<br>';
 ?>
