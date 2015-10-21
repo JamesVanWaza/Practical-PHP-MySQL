a819ce808649711a79316ad2316689846f157168
JamesVanWaza <jgacuca@gmail.com>
2015-10-21 19:37:54 -0400

    Added the index.php page

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<title>Practical PHP MySQL</title>
+</head>
+
+<body>
+<ul>
+	<li><a href="admintable/index.php">Admintable</a></li>
+	<li><a href="finalpost/index.php">Finalpost</a></li>
+	<li><a href="lmnmigrate/index.php">LMNMigrate</a></li>
+	<li><a href="loginDB/index.php">LoginDB</a></li>
+	<li><a href="postalDB/index.php">PostalDB</a></li>
+	<li><a href="simpleIDB/register-view-users.php">SimpleIDB</a></li>
+</ul>
+</body>
+</html>
