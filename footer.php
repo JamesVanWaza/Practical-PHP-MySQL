2657f2cd77deb4ea83387dbdf1d54a50842aec7f
JamesVanWaza <info@dosbranding.com>
2019-01-25 21:10:08 -0500

    Moved the scripts to the footer

--- footer.php
+++ footer.php
@@ -1,5 +1,8 @@
-	&copy; <?php /*Copyright of the Year*/echo date("Y") . "<br>"; ?>
-	<!--&copy; 2015 - --> <?//php echo date("Y");?>
+    <footer class="top-bar"><i class="fas fa-copyright fa-lg"></i> 2012 -  <?php echo date("Y") . "<br>"; ?>   </footer>
+
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="node_modules/what-input/dist/what-input.js"></script>
+    <script src="node_modules/foundation-sites/dist/js/foundation.min.js"></script>
 
 	<script>
     	$(document).foundation();
--- index.php
+++ index.php
@@ -1,14 +1,8 @@
 <?php require 'html5req.php';?>
 
 <body>
-  <script src="js/vendor/jquery.js"></script>
-  <script src="js/vendor/modernizr.js"></script>
-  <script src="js/vendor/fastclick.js"></script>
-  <script src="js/foundation.min.js"></script>
-    <script src="node_modules/"></script>
-
 <h1>PRACTICAL PHP MySQL</h1>
-<ul>
+<ul class="vertical menu">
 	<li><a href="admintable/index.php" target="_blank">Admintable</a></li>
 	<li><a href="finalpost/index.php" target="_blank">Finalpost</a></li>
 	<li><a href="lmnmigrate/index.php" target="_blank">LMNMigrate</a></li>
