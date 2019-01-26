edc157c22fd2828167215e455ff3ea521e29f919
JamesVanWaza <info@dosbranding.com>
2019-01-25 20:05:53 -0500

    Added the target_blank for the Links

--- index.php
+++ index.php
@@ -8,12 +8,12 @@
 
 <h1>PRACTICAL PHP MySQL</h1>
 <ul>
-	<li><a href="admintable/index.php">Admintable</a></li>
-	<li><a href="finalpost/index.php">Finalpost</a></li>
-	<li><a href="lmnmigrate/index.php">LMNMigrate</a></li>
-	<li><a href="loginDB/index.php">LoginDB</a></li>
-	<li><a href="postalDB/index.php">PostalDB</a></li>
-	<li><a href="simpleIDB/register-view-users.php">SimpleIDB</a></li>
+	<li><a href="admintable/index.php" target="_blank">Admintable</a></li>
+	<li><a href="finalpost/index.php" target="_blank">Finalpost</a></li>
+	<li><a href="lmnmigrate/index.php" target="_blank">LMNMigrate</a></li>
+	<li><a href="loginDB/index.php" target="_blank">LoginDB</a></li>
+	<li><a href="postalDB/index.php" target="_blank">PostalDB</a></li>
+	<li><a href="simpleIDB/register-view-users.php" target="_blank">SimpleIDB</a></li>
 </ul>
 
 <?php require 'footer.php';?>
