178410d40e3992971cacadd09ccd5fff9a5081d4
JamesVanWaza <info@dosbranding.com>
2019-01-25 21:35:13 -0500

    Added the Favicon

--- html5req.php
+++ html5req.php
@@ -5,6 +5,12 @@
   <link href="node_modules/foundation-sites/dist/css/foundation.min.css" rel="stylesheet" type="text/css">
   <link href="css/main.css" rel="stylesheet" type="text/css">
   <script defer src="node_modules/@fortawesome/fontawesome-free/js/all.js"></script>
+  <link rel="icon" type="image/png" href="images/php-mysql-logo@0.5x.png" sizes="256x256">
+  <link rel="icon" type="image/png" href="images/php-mysql-logo@0.75x.png" sizes="384x384">
+  <link rel="icon" type="image/png" href="images/php-mysql-logo.png" sizes="512x512">
+  <link rel="icon" type="image/png" href="images/php-mysql-logo@2x.png" sizes="1024x1024">
+  <link rel="icon" type="image/png" href="images/php-mysql-logo@3x.png" sizes="1536x1436">
+  <link rel="icon" type="image/png" href="images/php-mysql-logo@4x.png" sizes="2048x2048">
 </head>
 
 
