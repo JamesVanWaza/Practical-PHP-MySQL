1850046f172620261dfd53429040b4170826a97d
JamesVanWaza <info@dosbranding.com>
2019-01-28 14:24:49 -0500

    HTML5-Beautify

--- footer.php
+++ footer.php
@@ -1,4 +1,6 @@
-    <footer class="top-bar"><i class="fas fa-copyright fa-lg"></i> 2012 -  <?php echo date("Y") . "<br>"; ?>   </footer>
+    <footer class="top-bar">
+    	<i class="fas fa-copyright fa-lg"></i> 2012 -  <?php echo date("Y") . "<br>"; ?>
+    </footer>
 
     <script src="node_modules/jquery/dist/jquery.min.js"></script>
     <script src="node_modules/what-input/dist/what-input.js"></script>
