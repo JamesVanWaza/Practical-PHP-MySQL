62d7e4bedfa12a439fbfe06706cd1089c33b675e
JamesVanWaza <info@dosbranding.com>
2019-02-08 20:08:33 -0500

    HTML5Beautified

--- loginDB/register-page.php
+++ loginDB/register-page.php
@@ -88,54 +88,59 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <h2 class="text-center">Register</h2>
 <!--display the form on the screen-->
 <form action="register-page.php" method="post">
-  <div class="row"><!--Beginning of First Row-->
-  	<div class="large-6 medium-6 small-12 columns">
-      <label>First Name
-        <input id="fname" type="text" name="fname" size="30" maxlength="30" placeholder="First Name" value="<?php if (isset($_POST['fname'])) {
+  	<div class="row"><!--Beginning of First Row-->
+  		<div class="large-6 medium-6 small-12 columns">
+      		<label>First Name
+        		<input id="fname" type="text" name="fname" size="30" maxlength="30" placeholder="First Name" value="<?php if (isset($_POST['fname'])) {
 	echo $_POST['fname'];
 }
 ?>"/>
-      </label>
-    </div>
-     <div class="large-6 medium-6 small-12 columns">
-      <label>Last Name
-        <input id="lname" type="text" name="lname" size="40" maxlength="40" placeholder="Last Name" value="<?php if (isset($_POST['lname'])) {
+      		</label>
+    	</div>
+
+     	<div class="large-6 medium-6 small-12 columns">
+      		<label>Last Name
+        		<input id="lname" type="text" name="lname" size="40" maxlength="40" placeholder="Last Name" value="<?php if (isset($_POST['lname'])) {
 	echo $_POST['lname'];
 }
 ?>"/>
-      </label>
-    </div>
-  </div><!--End of First Row-->
-  <div class="row"><!--Beginning of Second Row-->
-    <div class="large-12 small-12 columns">
-      <label>Email
-         <input id="email" type="email" name="email" size="50" maxlength="50" placeholder="Email" value="<?php if (isset($_POST['email'])) {
+      		</label>
+    	</div>
+  	</div><!--End of First Row-->
+
+  	<div class="row"><!--Beginning of Second Row-->
+    	<div class="large-12 small-12 columns">
+      		<label>Email
+         		<input id="email" type="email" name="email" size="50" maxlength="50" placeholder="Email" value="<?php if (isset($_POST['email'])) {
 	echo $_POST['email'];
 }
 ?>"/>
-      </label>
-    </div>
-  </div><!--End of Second Row-->
-  <div class="row"><!--Beginning of Third Row-->
-    <div class="large-6 medium-6 small-12 columns">
-      <label>Password
-        <input id="psword1" type="password" name="psword1" size="12" maxlength="12" placeholder="Password" value="<?php if (isset($_POST['psword1'])) {
+      		</label>
+    	</div>
+  	</div><!--End of Second Row-->
+
+	<div class="row"><!--Beginning of Third Row-->
+	    <div class="large-6 medium-6 small-12 columns">
+	      	<label>Password
+	        	<input id="psword1" type="password" name="psword1" size="12" maxlength="12" placeholder="Password" value="<?php if (isset($_POST['psword1'])) {
 	echo $_POST['psword1'];
 }
 ?>"/>
-      </label>
-    </div>
-    <div class="large-6 medium-6 small-12 columns">
-      <label>Confirm Password
-        <input id="psword2" type="password" name="psword2" size="12" maxlength="12" placeholder="Confirm Password" value="<?php if (isset($_POST['psword2'])) {
+	      	</label>
+	    </div>
+
+	    <div class="large-6 medium-6 small-12 columns">
+	      	<label>Confirm Password
+	        	<input id="psword2" type="password" name="psword2" size="12" maxlength="12" placeholder="Confirm Password" value="<?php if (isset($_POST['psword2'])) {
 	echo $_POST['psword2'];
 }
 ?>"/>
-      </label>
-      </div>
-      <div class="large-12 small-12 columns">
-  	<input type="submit" id="submit" name="submit" class="button [radius round]" value="Register">
-  </div>
+	      	</label>
+	    </div>
+  	</div>
+
+    <div class="large-12 small-12 columns">
+  		<input type="submit" id="submit" name="submit" class="button [radius round]" value="Register">
     </div><!--End of Third Row-->
 </form>
 <?php include 'footer.php';?>
